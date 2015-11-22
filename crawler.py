67dd52d7f68142d068ba752c544294b5476d9070
admin <admin>
2015-11-22 23:51:31 +0200

    few steps further

--- crawler.py
+++ crawler.py
@@ -9,12 +9,26 @@ voting_days = [elem.get_attribute("href") for elem in voting_days]
 for link in voting_days:
     driver.get(link)
     laws = driver.find_elements_by_xpath('//div[@class="nomer"]/a')
-    law_numbers = [elem.get_attribute("text") for elem in laws]
-    law_links = [elem.get_attribute("href") for elem in law_numbers if elem.get_attribute("text") != '\xa0']
+    law_numbers = [elem.get_attribute("text") for elem in laws if elem.get_attribute("text") != '\xa0']
+    law_links = [elem.get_attribute("href") for elem in laws if elem.get_attribute("text") != '\xa0']
 
     for link in law_links:
-        url4 = url3 + "#ui-tabs-2"
-        url4.replace(" ", "")
-        driver.get(link)
-        voting_list = driver.find_elements_by_xpath('//div[@class="fr_nazva"]/a/text()')
-        voting_l = driver.find_elements_by_xpath('//div[@class="fr_nazva"]/a')
+        url = link + "#ui-tabs-2"
+        driver.get(url)
+        voting_list = driver.find_elements_by_xpath('//div[@class="fr_nazva"]/a')
+        voting_names = [elem.get_attribute("text") for elem in voting_list]
+        voting_urls = [elem.get_attribute("href") for elem in voting_list]
+        expand = driver.find_elements_by_xpath("//div[@class='vid_d']/p[@id='name_input']")
+        expand[0].click()
+
+        trials = driver.find_elements_by_xpath('//div[@class="block_pd"]')
+        for elem in trials:
+            trail_numbers = [elem.get_attribute("innerHTML") for elem in trials_num]
+
+
+        trials_num = driver.find_elements_by_xpath('//div[@class="fr_nomer"]')
+        trail_numbers = [elem.get_attribute("innerHTML") for elem in trials_num]
+        trail_numbers = [elem for elem in trail_numbers if '-' not in elem]
+        trail_names = [elem.get_attribute("text") for elem in trials if 'про проект Закону' in elem.get_attribute("text")]
+
+
