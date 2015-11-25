4f323f4823a4263463799fbe40a2804217aaf018
Evginia Kopp <strizhakova@gmail.com>
2015-11-25 19:00:48 +0200

    25-11-2015

--- crawler.py
+++ crawler.py
@@ -42,3 +42,32 @@ for link in voting_days:
 
 # [num for num, law in zip(nums, laws) if "x" in law]
 # ".//*[@id='list_g']"
+deps = driver.find_elements_by_xpath("//table[@class='tab_gol']/tbody/tr/td[2]") # deputies
+dep_list = [elem.get_attribute("innerHTML") for elem in deps]
+votes = driver.find_elements_by_xpath("//table[@class='tab_gol']/tbody//td[3]")
+colored_votes = []
+for vote in votes:
+    #get element by color
+    color = vote.get_attribute("style").replace("color: ", "").replace(";", "")
+    if not color:  # this means '-' is  in the cell
+        if vote.text == '•':
+            color = 'didnotvote'
+        else:
+            color = 'absent'
+    
+    colored_votes.append(color)
+    # if votes[elem].find_elements_by_xpath("//tr[elem]/td[@style='color:Yellow;']"):
+    #     votes[elem] = "utrymavsya"
+    # elif votes[elem].find_elements_by_xpath("//tr[elem]/td[@style='color:green;']"):
+    #     votes[elem] = "za"
+    # elif votes[elem].find_elements_by_xpath("//tr[elem]/td[@style='color:red;']"):
+    #     votes[elem] = "proty"
+    # else:
+    #     votes[elem] = "vidsutniiy"
+dep_list = [elem.get_attribute("innerHTML") for elem in deps]
+
+for i in range(len(trails_table)):
+    count = trails_table[i][0]
+
+if votes[2].find_elements_by_xpath("//tr[2]/td[@style='color:Yellow;']"):
+    elem = "utrymavsya"
